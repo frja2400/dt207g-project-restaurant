c6252f5af0bb5a235890ec4e636b7e841743e8f1
Frida Jansson <frja2400@student.miun.se>
2025-05-28 11:46:06 +0200

    Skapat asynkron funktion för att hämta menydata från mitt API. Visar laddningsmeddelande medan data hämtas. Renderar menyn dynamiskt i DOM. Visar vegansk symbol om isVegan är true. Adderat beställ-knapp på varje menyobjekt.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,50 @@
 "use strict"
+
+document.addEventListener('DOMContentLoaded', getData);
+
+function showLoadingMessage() {
+    const menuEl = document.getElementById("menuContainer");
+    menuEl.innerHTML = `<h3>Laddar menyn...</h3>`;
+}
+
+//Asynkron funktion med try/catch för att hämta ut min data. 
+async function getData() {
+    showLoadingMessage();
+    try {
+
+        const response = await fetch("https://dt207g-project-restapi.onrender.com/api/menu");
+
+        if (!response.ok) throw new Error("Nätverksfel");
+
+        const data = await response.json();
+        console.table(data);
+        renderData(data);   //Anropar funktion som skriver ut datan på skärmen.
+    } catch (error) {
+        console.error("Fel vid hämtning:", error);
+    } finally {
+        console.log("Förfrågan avslutad.");
+    }
+}
+
+//Skriv ut menyn på skärmen
+function renderData(data) {
+    const menuEl = document.getElementById("menuContainer");
+    menuEl.innerHTML = '';
+
+    data.forEach(menuItem => {
+        const row = document.createElement('div');
+        row.classList.add('menuItem');
+
+        const veganIcon = menuItem.isVegan ? 'Vegansk 🌱' : '';
+
+        row.innerHTML = `
+            <h3>${menuItem.name}</h3>
+            <p>${menuItem.description}</p>
+            <p><strong>Pris:</strong> ${menuItem.price} kr</p>
+            <p class="vegan">${veganIcon}</p>
+            <button class="addBtn">BESTÄLL</button>
+        `;
+
+        menuEl.appendChild(row);
+    });
+}
