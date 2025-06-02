e37aab4d9cd46d9793bce5081a7fe87e7301ff69
Frida Jansson <frja2400@student.miun.se>
2025-06-02 13:01:08 +0200

    Flyttar min varukorg utanför headern och gör den fixed så att den ska följa med i hörnet när man scrollar.

--- src/js/main.js
+++ src/js/main.js
@@ -67,7 +67,7 @@ function renderData(data) {
 
             row.innerHTML = `
                 <h4>${menuItem.name}</h4>
-                <p class="description"><i>${menuItem.description}</i></p>
+                <p class="description"><em>${menuItem.description}</em></p>
                 <p><strong>Pris:</strong> ${menuItem.price} kr</p>
                 <p class="vegan">${veganIcon}</p>
             `;
